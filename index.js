bbaa6c6ec823077186435a9874f63145682655f4
maureen <maureen.chelangat@student.moringaschool.com>
2023-09-17 18:35:52 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove();
+  }
+
+  const newHeader = document.createElement("h1");
+  newHeader.id = 'victory';
+  newHeader.textContent = Keter + " is the champion";
+  document.body.appendChild(newHeader);
+
+  const nwHeader = document.createElement('h1');
+  nwHeader.id = 'victory';
+  const yourName = 'Keter'; 
+  nwHeader.textContent = `${yourName} is the champion`;
+        
+  
+
+
+
+    
+    
+    
